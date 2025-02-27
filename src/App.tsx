65d76405c3fa5602526261d23432cacf2c30a6c6
Meng Siau <hsia0003@student.monash.edu>
2025-02-27 18:02:47 +1100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is hello {count}
+          The count has completely vchanged  {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
